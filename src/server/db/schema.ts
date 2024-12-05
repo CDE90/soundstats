e07aa523324dbcd5c9375e7aa4c27620a6c70e68
CDE90 <ethan@ethancoward.dev>
2024-12-05 20:58:51 +0000

    Basic formatting pass

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -3,11 +3,11 @@
 
 import { sql } from "drizzle-orm";
 import {
-  index,
-  integer,
-  pgTableCreator,
-  timestamp,
-  varchar,
+    index,
+    integer,
+    pgTableCreator,
+    timestamp,
+    varchar,
 } from "drizzle-orm/pg-core";
 
 /**
@@ -16,21 +16,21 @@ import {
  *
  * @see https://orm.drizzle.team/docs/goodies#multi-project-schema
  */
-export const createTable = pgTableCreator((name) => `web-spotify-thing_${name}`);
+export const createTable = pgTableCreator((name) => `spotify-thing_${name}`);
 
 export const posts = createTable(
-  "post",
-  {
-    id: integer("id").primaryKey().generatedByDefaultAsIdentity(),
-    name: varchar("name", { length: 256 }),
-    createdAt: timestamp("created_at", { withTimezone: true })
-      .default(sql`CURRENT_TIMESTAMP`)
-      .notNull(),
-    updatedAt: timestamp("updated_at", { withTimezone: true }).$onUpdate(
-      () => new Date()
-    ),
-  },
-  (example) => ({
-    nameIndex: index("name_idx").on(example.name),
-  })
+    "post",
+    {
+        id: integer("id").primaryKey().generatedByDefaultAsIdentity(),
+        name: varchar("name", { length: 256 }),
+        createdAt: timestamp("created_at", { withTimezone: true })
+            .default(sql`CURRENT_TIMESTAMP`)
+            .notNull(),
+        updatedAt: timestamp("updated_at", { withTimezone: true }).$onUpdate(
+            () => new Date(),
+        ),
+    },
+    (example) => ({
+        nameIndex: index("name_idx").on(example.name),
+    }),
 );
