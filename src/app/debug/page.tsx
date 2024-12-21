4aef16f49ebd76436ac23188096460f4b36a0174
CDE90 <ethan@ethancoward.dev>
2024-12-21 20:12:57 +0000

    Reworking pages

---
+++ src/app/debug/page.tsx
@@ -0,0 +1,31 @@
+import { getCurrentlyPlaying } from "@/server/spotify/spotify";
+import { auth, clerkClient } from "@clerk/nextjs/server";
+
+export default async function DebugPage() {
+    const { userId } = await auth();
+
+    if (!userId) {
+        return <p>You are not signed in.</p>;
+    }
+
+    const clerkTokenResponse = await (
+        await clerkClient()
+    ).users.getUserOauthAccessToken(userId, "oauth_spotify");
+
+    if (!clerkTokenResponse.data) {
+        return <p>Error getting access token.</p>;
+    }
+
+    const data = clerkTokenResponse.data[0]!;
+    const accessToken = data.token;
+
+    const nowListening = await getCurrentlyPlaying(accessToken);
+
+    return (
+        <div className="p-4">
+            <p>Your user ID is {userId}</p>
+            <p>Listening to {nowListening?.item.name}</p>
+            <pre>{JSON.stringify(nowListening, null, 2)}</pre>
+        </div>
+    );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,7 @@
-import { auth, clerkClient } from "@clerk/nextjs/server";
-import { getCurrentlyPlaying } from "@/server/spotify/spotify";
-
-export default async function HomePage() {
-    const { userId } = await auth();
-
-    if (!userId) {
-        return <p>You are not signed in.</p>;
-    }
-
-    const clerkTokenResponse = await (
-        await clerkClient()
-    ).users.getUserOauthAccessToken(userId, "oauth_spotify");
-
-    if (!clerkTokenResponse.data) {
-        return <p>Error getting access token.</p>;
-    }
-
-    const data = clerkTokenResponse.data[0]!;
-    const accessToken = data.token;
-
-    const nowListening = await getCurrentlyPlaying(accessToken);
-
+export default function HomePage() {
     return (
         <div className="p-4">
             <h1>Welcome!</h1>
-            <p>Your user ID is {userId}</p>
-            <p>Listening to {nowListening?.item.name}</p>
-            <p>{JSON.stringify(nowListening)}</p>
         </div>
     );
 }
